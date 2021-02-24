651392316b0ae7bbff03c0e24d5665cc9d89aefc
Nicholas Noochla-or <nicholasn@Nicholass-MacBook.local>
2021-02-24 07:42:51 -0800

    completed solution

--- python/valAdd.py
+++ python/valAdd.py
@@ -1,9 +1,10 @@
 def add_prices(basket):
-    total = 0
+    total = round(0,2)
     for key in basket:
-        total = total + basket[key]
-        print(total)
+        total = round(total, 2) + round(basket[key], 2)
+        print(round(total, 2))
 
 
-groceries = {"bananas": 1.56, "apples": 2.50, "oranges": 0.99, "bread": 4.59, "coffee": 6.99, "milk": 3.99, "eggs": 2.98, "cheese": 5.44}
+groceries = {"bananas": 1.56, "apples": 2.50, "oranges": 0.99, "bread": 4.59, 
+	"coffee": 6.99, "milk": 3.39, "eggs": 2.98, "cheese": 5.44}
 add_prices(groceries)
